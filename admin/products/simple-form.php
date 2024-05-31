545b381c5e320b1fb7e6295c9d7869094cb9d231
GURLEEN KAUR <137621325+kaurguru@users.noreply.github.com>
2024-05-31 19:01:10 +0530

    Create simple-form.php

---
+++ admin/products/simple-form.php
@@ -0,0 +1,89 @@
+<h1> add new product</h1>
+    <div class="form">
+        <form action="<?php $_PHP_SELF ?>" method="post" >
+          
+             <div class="first">
+                <div><label>discount:</label> </div>
+            <div>
+            <input type="number" name="discount" required placeholder="discount" >
+            </div>
+            
+            </div>
+
+            <div class="first">
+                <div>
+                <label>product image:</label>
+                </div>
+                <div>
+                <input type="file" name="image"  required>
+                </div>
+           
+            </div>
+
+            <div class="first">
+                <div>  <label>name/ description:</label></div>
+                <div>
+                <input type="text" name="name" required placeholder="description" >
+                </div>
+
+            </div>
+
+            <div class="first">
+                <div>
+                <label>gender:</label>
+                </div>
+                <div>
+                    <input type="radio" name="gender" value="men"> 
+                    <span>men</span>
+                    <input type="radio" name="gender" value="women">
+                    <span>women</span>
+                </div>
+           </div>
+
+            <div class="first">
+                <div>
+                <label>category:</label> 
+                </div>
+                <div>
+                <select name="category" id="category" required>
+                        <option value="bangles">bangles</option>
+                        <option value="earrings">earrings</option>
+                        <option value="chains">chains</option>
+                        <option value="necklaces">necklaces</option>
+                        <option value="rings">rings</option>
+                        <option value="bracelets">bracelets</option>
+                        <option value="pendants">pendants</option>
+                        <option value="gifts">gifts</option>
+                    </select>
+                </div>
+                    
+            </div>
+
+               
+            <div class="first">
+                <div>
+                <label>price:</label>
+                </div>
+                <div>
+                <input type="number" name="price" placeholder="price"  required>
+                </div>
+            
+            </div>
+
+            <div class="first">
+                <div><label>Quantity:</label> </div>
+                <div>
+                <input type="number" name="quantity" required placeholder="Quantity" >
+                </div>
+            </div>
+            
+            <div>
+
+                <input type="submit" value="submit" name="submit2" class="submit">
+            </div>
+        </form>
+    </div>
+  
+  
+
+
